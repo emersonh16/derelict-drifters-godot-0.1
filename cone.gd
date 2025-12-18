ee347825da332fee044d6f48d5df4c3661aab2b4
Emerson Hendry <emersonh16@gmail.com>
2025-12-18 17:06:08 -0500

    i need a break

--- cone.gd
+++ cone.gd
@@ -1,11 +1,16 @@
 extends Polygon2D
 
+@onready var hitbox := $"../Hitbox"
+
 @export var length := 120.0
 @export var width := 40.0
 
 func _ready():
-	polygon = [
+	var pts = [
 		Vector2(0, 0),
 		Vector2(length, -width * 0.5),
 		Vector2(length,  width * 0.5),
 	]
+
+	polygon = pts
+	hitbox.polygon = pts
